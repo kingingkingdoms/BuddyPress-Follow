535878d4b61c5aa5f9decd5dace1c12d99fb2605
kingingkingdoms <37118190+kingingkingdoms@users.noreply.github.com>
2018-07-02 17:17:37 -0400

    Update bp-total-followers-widgets.php

--- _inc/bp-total-followers-widgets.php
+++ _inc/bp-total-followers-widgets.php
@@ -1,31 +1,13 @@
 <?php
-/**
- * BP Total Followers Widget
- *
- * @package BP-Follow
- * @subpackage Widgets
- */
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
-/**
- * Add a "BuddyPress Total Followers" widget for the displayed user
- *
- * @subpackage Widgets
- */
-/* BuddyPress Total Followers Count Widget */
-class Total_Followers_Widget extends WP_Widget {
+class Total_Follower_Widget extends WP_Widget {
 	public function __construct() {
-		$widget_ops = array('classname' => 'Total_Followers_Widget', 'description' => 'Display the total number of followers a displayed user has on their profile as a widget.' );
-		$this->WP_Widget('Total_Followers_Widget', 'BuddyPress Total Followers', $widget_ops);
+		$widget_ops = array('classname' => 'Total_Follower_Widget', 'description' => 'Display the total number of followers a displayed user has as a widget.' );
+		$this->WP_Widget('Total_Follower_Widget', 'BuddyPress Total Followers Count 2', $widget_ops);
 	}
 	function widget($args, $instance) {
-		// PART 1: Extracting the arguments + getting the values
+		//PART 1: Extracting the arguments + getting the values
 		extract($args, EXTR_SKIP);
 		
-		$title = empty( $instance['title'] ) ? ' ' : apply_filters('widget_title', $instance['title']);
-		$subject_single = __( 'person' , 'Total_Followers_Widget' );
-		$subject_plural = __( 'people' , 'Total_Followers_Widget' );
-		$number = bpfw_format_count( $count );
 		if ( ! function_exists( 'bpfw_format_count' ) ) {
 			function bpfw_format_count( $number ) {
 				$precision = 2;
@@ -38,15 +20,19 @@ class Total_Followers_Widget extends WP_Widget {
 				if ( $number >= 1000000000 ) {
 					$formatted = number_format( $number / 1000000000, $precision ) . 'B';
 				} else {
-					$formatted = $number; //Number is less than 1000
+					$formatted = $number; // Number is less than 1000
 				}
 				$formatted = str_replace( '.00', '', $formatted );
 				return $formatted;
 			}
 		}
+		$title = empty( $instance['title'] ) ? ' ' : apply_filters('widget_title', $instance['title']);
+		$subject_single = __( 'person' , 'Total_Follower_Widget' );
+		$subject_plural = __( 'people' , 'Total_Follower_Widget' );
+		$number = bpfw_format_count( $count );
 		echo (isset($before_widget)?$before_widget:'');
 		
-		//PART 2: The title and output
+		//PART 2: The title and the text output
 		if (!empty($title) )
 			echo $before_title . $title . $after_title;
 			$count = bp_follow_total_follow_counts( array(
@@ -66,17 +52,17 @@ class Total_Followers_Widget extends WP_Widget {
 	
 	//PART 3: The backend form and settings
 	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Total Followers' ) );
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
 		$title = $instance['title'];
 		?>
-		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>">Title:
-				<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
+			<p>
+				<label for="<?php echo $this->get_field_id('title'); ?>">Title:
+					<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>"
 			   name="<?php echo $this->get_field_name('title'); ?>" type="text"
                value="<?php echo attribute_escape($title); ?>" />
-			</label>
-		</p>
-	<?php
+				</label>
+			</p>
+		<?php
 	}
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
@@ -85,4 +71,4 @@ class Total_Followers_Widget extends WP_Widget {
 		return $instance;
 	}
 }
-add_action( 'widgets_init', create_function('', 'return register_widget("Total_Followers_Widget");') );
+add_action( 'widgets_init', create_function('', 'return register_widget("Total_Follower_Widget");') );
